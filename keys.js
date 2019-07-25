cc718c92cfdd17f79e360c2dcb7919c42ea79e73
Ryan Ward <ryanward4556@gmail.com>
2019-07-25 13:20:42 -0400

    added spotify id and client secret, also installed node spotify package

--- keys.js
+++ keys.js
@@ -1,8 +1,20 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+    id: process.env.f53861f31859458f8fb64687eff0389f,
+    secret: process.env.bb6684a34faf47eabb6cefa5a5d83b72
 };
 
-var keys = require("./keys.js");
+exports.concert = {
+    id: process.env.CONCERT_ID,
+    secret: process.env.CONCERT_SECRET
+};
+
+exports.movie = {
+    id: process.env.MOVIE_ID,
+    secret: process.env.MOVIE_SECRET
+};
+
+
+
+
--- liri.js
+++ liri.js
@@ -1,3 +1,50 @@
 require("dotenv").config();
 
+
+var keys = require("./keys.js");
+
 var spotify = new Spotify(keys.spotify);
+
+
+// concert-this
+// node liri.js concert-this <artist/band name here>
+
+// Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+// ----------------------------------------------------------------------------
+
+// spotify-this-song
+// node liri.js spotify-this-song '<song name here>'
+
+// If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+
+// ----------------------------------------------------------------------------
+
+// movie-this
+// node liri.js movie-this '<movie name here>'
+
+
+// * Title of the movie.
+// * Year the movie came out.
+// * IMDB Rating of the movie.
+// * Rotten Tomatoes Rating of the movie.
+// * Country where the movie was produced.
+// * Language of the movie.
+// * Plot of the movie.
+// * Actors in the movie.
+
+
+// ----------------------------------------------------------------------------
+
+// do-what-it-says
+// node liri.js do-what-it-says
+
+
+
