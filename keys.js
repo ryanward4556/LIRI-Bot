88bca01aca0bbf16c10a4c07b8270459a28d250f
Ryan Ward <ryanward4556@gmail.com>
2019-07-25 13:22:53 -0400

    fixed keys

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.spotify = {
-    id: process.env.f53861f31859458f8fb64687eff0389f,
-    secret: process.env.bb6684a34faf47eabb6cefa5a5d83b72
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
 };
 
 exports.concert = {
